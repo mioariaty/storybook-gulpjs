30e61c53b565287a591dbcc30a54018f675029ab
mioriaty <mioriaty@gmail.com>
2020-08-18 12:13:46 +0700

    Add: component tab

---
+++ src/assets/js/tab.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Tab = function (options) {
+  // select tab element
+  const el = document.querySelector(options.el);
+  const tabLinks = el.querySelectorAll(options.tabLinks);
+  const tabPanes = el.querySelectorAll(options.tabPanes);
+
+  console.log(el);
+  let activeIndex = 0;
+  let initCalled = false;
+
+  // init
+  const render = () => {
+    if (!initCalled) {
+      initCalled = true;
+      el.classList.remove('no-js');
+      tabLinks.forEach((link, index) => _handleClick(link, index));
+    }
+  };
+
+  const _handleClick = (link, index) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      goToTab(index);
+    });
+  };
+
+  // go to current tab pane
+  const goToTab = (index) => {
+    if (index !== activeIndex && index >= 0 && index <= tabLinks.length) {
+      tabLinks[activeIndex].classList.remove('is-active');
+      tabLinks[index].classList.add('is-active');
+
+      tabPanes[activeIndex].classList.remove('is-active');
+      tabPanes[index].classList.add('is-active');
+
+      activeIndex = index;
+    }
+  };
+
+  return {
+    render: render,
+    goToTab: goToTab,
+  };
+};
+
+// export global namespace
+// window.Tab = Tab;
+// module.exports = Tab;
+
+export default Tab;
+
+// const testTab = Tab({
+//   el: '.tab-wrapper',
+//   navLinks: '.tab__link',
+//   navPanes: '.tab__pane',
+// }).render;
+// testTab.render();
---
+++ src/stories/Tab.stories.js
@@ -0,0 +1,83 @@
+import Tab from '../assets/js/tab';
+
+export default {
+  title: 'Components/Tab',
+};
+
+export const Horizontal = () => {
+  return /* html */ `
+    <div id="tab-demo"  class="tab-wrapper tab--horizontal">
+      <ul class="tab__nav">
+        <li class="tab__item">
+          <a href="#" class="tab__link is-active">tab link 1</a>
+        </li>
+        <li class="tab__item">
+          <a href="#" class="tab__link">tab link 2</a>
+        </li>
+        <li class="tab__item">
+          <a href="#" class="tab__link">tab link 3</a>
+        </li>
+      </ul>
+
+      <div class="tab__content">
+        <div class="tab__pane is-active">
+          <p>
+            content thứ 1
+          </p>
+        </div>
+        <div class="tab__pane">
+          <p>
+            content thứ 2
+          </p>
+        </div>
+        <div class="tab__pane">
+          <p>
+            content thứ 3
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+};
+// const testTab = Tab({
+//   el: '#tab-demo',
+//   navLinks: '.tab__link',
+//   navPanes: '.tab__pane',
+// });
+// testTab.render();
+
+export const Vertical = () => {
+  return /*html*/ `
+    <div class="tab-wrapper tab--vertical">
+      <ul class="tab__nav">
+        <li class="tab__item">
+          <a href="#" class="tab__link is-active">tab link 1</a>
+        </li>
+        <li class="tab__item">
+          <a href="#" class="tab__link">tab link 2</a>
+        </li>
+        <li class="tab__item">
+          <a href="#" class="tab__link">tab link 3</a>
+        </li>
+      </ul>
+
+      <div class="tab__content">
+        <div class="tab__pane is-active">
+          <p>
+            content thứ 1
+          </p>
+        </div>
+        <div class="tab__pane">
+          <p>
+            content thứ 2
+          </p>
+        </div>
+        <div class="tab__pane">
+          <p>
+            content thứ 3
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+};
