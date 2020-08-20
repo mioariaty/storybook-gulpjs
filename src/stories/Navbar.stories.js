80b7e04e05c99b342f4c2f6a7c7caf97866297c5
mioriaty <mioriaty@gmail.com>
2020-08-20 11:27:44 +0700

    Add navbar component

---
+++ src/stories/Navbar.stories.js
@@ -0,0 +1,48 @@
+export default {
+  title: 'Components/Navbar',
+};
+
+export const Default = () => {
+  return /* html */ `
+    <nav class="navbar">
+      <a href="#" class="navbar__brand"><span>B</span>M.</a>
+      <button class="navbar__toggler">
+        <i class="far fa-bars"></i>
+      </button>
+
+      <div class="navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav__item">
+            <a href="#" class="nav__link">Home</a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">About</a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">Portfolio</a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">Blog</a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">Contact</a>
+          </li>
+        </ul>
+
+        <form class="form form--horizontal">
+          <div class="form-item row m-0">
+            <input type="search" placeholder="Search" class="form-item__input input--rounded input--search" >
+            <button class="btn btn--search">
+              <i class="fal fa-search"></i>
+            </button>
+          </div>
+        </form>
+
+      </div>
+    </nav>
+  `;
+};
