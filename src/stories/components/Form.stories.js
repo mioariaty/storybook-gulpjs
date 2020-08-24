61bfa763e9f9017116de92a0fa51068e8263771f
mioriaty <mioriaty@gmail.com>
2020-08-24 17:41:43 +0700

    fix form component

--- src/stories/components/Form.stories.js
+++ src/stories/components/Form.stories.js
@@ -5,15 +5,19 @@ export default {
 export const Horizontal = () => {
   return /*html*/ `
     <form class="form form--horizontal">
-      <div class="form-item row">
-        <label class="form-item__label col-desk-3">Email</label>
-        <input type="email" class="input-control col-desk-9" >
+      <div class="form__item row">
+        <label class="form__label col-desk-2">Email</label>
+        <input type="email" class="input-control col-desk-10" >
       </div>
-      <div class="form-item row">
-        <label class="form-item__label col-desk-3">Password</label>
-        <input type="password" class="input-control col-desk-9" >
+      <div class="form__item row">
+        <label class="form__label col-desk-2">Password</label>
+        <input type="password" class="input-control col-desk-10" >
       </div>
-      <button class="btn btn--rounded mx-auto">
+      <div class="form__item row">
+        <label class="form__label col-desk-2">Message</label>
+        <textarea name="" id="" cols="30" rows="10" class="input-control col-desk-10"></textarea>
+      </div>
+      <button class="btn btn--rounded btn--secondary mx-auto">
         Submit
       </button>
     </form>
@@ -23,15 +27,15 @@ export const Horizontal = () => {
 export const Vertical = () => {
   return /* html */ `
     <form class="form form--vertical">
-      <div class="form-item row">
-        <label class="form-item__label ">Email</label>
+      <div class="form__item row">
+        <label class="form__label ">Email</label>
         <input type="email" class="input-control" >
       </div>
-      <div class="form-item row">
-        <label class="form-item__label">Password</label>
+      <div class="form__item row">
+        <label class="form__label">Password</label>
         <input type="password" class="input-control" >
       </div>
-      <button class="btn btn--rounded mx-auto">
+      <button class="btn btn--rounded  mx-auto">
         Submit
       </button>
     </form>
