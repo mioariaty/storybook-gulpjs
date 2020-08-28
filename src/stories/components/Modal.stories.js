9e904d2344c1b0ade4479313367065e059451827
mioriaty <mioriaty@gmail.com>
2020-08-28 15:00:23 +0700

    add modal component

---
+++ src/stories/components/Modal.stories.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Components/Modal',
+};
+export const Default = () => {
+  return /*html*/ `
+    <button id="trigger" class="btn btn--rounded trigger-button" type="button">Launch Modal</button>
+
+    <div id="test-modal" class="modal">
+      <h1>It's Duonggggggggggg!</h1>
+      <p>Eyyyyyyyyyyyyyyyyyyyy</p>
+    </div>
+  `;
+};
