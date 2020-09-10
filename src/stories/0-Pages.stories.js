945d73ec0911b08afa7b27238b08dfccafc44870
Duongw Too <mioriaty@gmail.com>
2020-09-10 15:26:56 +0700

    config

--- src/stories/0-Pages.stories.js
+++ src/stories/0-Pages.stories.js
@@ -1,10 +1,6 @@
 import getBody from './utils';
-import home from '!source-loader!../index.html';
 
 export default {
   title: 'Pages',
 };
 
-export const Home = () => {
-  return getBody(home);
-};
